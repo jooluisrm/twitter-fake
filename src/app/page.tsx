67d903e6b2d8c783350979a5eeee50d977f5b8c3
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-09-02 08:49:18 -0300

    Adicionando tema dark

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import { ThemeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
 
 export const Page = () => {
     return (
         <div>
             <Button>Aperte</Button>
+            <ThemeToggle></ThemeToggle>
         </div>
     );
 }
