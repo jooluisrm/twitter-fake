fa6b4b3f85be426c8211c58ebb9d28860f09709c
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-09-05 10:45:25 -0300

    Agora os posts ficam armazenado no cache do navegador & foi criado uma nova função para add posts com a tecla enter do teclado

--- src/components/Tabela-Inicial/Home/Conteudo-home.tsx
+++ src/components/Tabela-Inicial/Home/Conteudo-home.tsx
@@ -3,22 +3,39 @@ import { Input } from "@/components/ui/input";
 import { IconeInput } from "./Icones-Input";
 import { Button } from "@/components/ui/button";
 import { PostModelo } from "@/components/Posts/Posts-modelo";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Posts } from "@/data/posts";
 import { useToast } from "@/hooks/use-toast";
 import { ToastAction } from "@/components/ui/toast";
+import { Post } from "@/types/postsType";
 
+
+const STORAGE_KEY = 'postListCache';
 
 
 
 export const ConteudoHome = () => {
 
-    const [listaPosts, setListaPosts] = useState([...Posts]);
+    const [listaMeusPosts, setListaPosts] = useState<Post[]>(JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]'));
+    const [listaPosts2, setListaPosts2] = useState<Post[]>([...Posts]);
+
+
     const [novoPost, setNovoPost] = useState('');
-    const { toast } = useToast()
+    const { toast } = useToast();
     const [carregando, setCarregando] = useState(false);
 
-    const Addpost = () => {
+    useEffect(() => {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(listaMeusPosts));
+    }, [listaMeusPosts]);
+
+    useEffect(() => {
+        setCarregando(true);
+        setTimeout(() => {
+            setCarregando(false);
+        }, 1500)
+    }, [])
+
+    const AddPost = () => {
         if (novoPost.length === 0) {
             return toast({
                 title: 'Não foi possivel fazer a publicação!',
@@ -26,9 +43,6 @@ export const ConteudoHome = () => {
                 action: <ToastAction altText="fechar">Fechar</ToastAction>
             });
         };
-
-        
-
         const getCurrentDate = (): string => {
             const date = new Date();
             const day = String(date.getDate()).padStart(2, '0'); // Pega o dia e garante dois dígitos
@@ -38,34 +52,83 @@ export const ConteudoHome = () => {
             return `${year}-${month}-${day}`; // Formata a data no padrão Dia-Mês-Ano
         };
         setCarregando(true);
-        setTimeout(() => setListaPosts([{
-            id: listaPosts.length + 1,
-            nome1: 'João Luis',
-            nome2: '@jao.luisrm_',
-            inicial: 'JL',
-            data: getCurrentDate(),
-            conteudo: novoPost,
-            img: 'https://avatars.githubusercontent.com/u/124599?v=4',
-            datas: {
-                comentarios: 0,
-                republicacao: 0,
-                likes: 0,
-                viws: 0,
-                compartilhamentos: 0
-            }
-        }, ...listaPosts]), 1500);
+
+        setListaPosts([
+            {
+                id: listaMeusPosts.length + 1,
+                nome1: 'João Luis',
+                nome2: '@jao.luisrm_',
+                inicial: 'JL',
+                data: getCurrentDate(),
+                conteudo: novoPost,
+                img: 'https://avatars.githubusercontent.com/u/124599?v=4',
+                datas: {
+                    comentarios: 0,
+                    republicacao: 0,
+                    likes: 0,
+                    viws: 0,
+                    compartilhamentos: 0
+                }
+            }, ...listaMeusPosts]);
+
         toast({
             title: 'Post postado com sucesso!',
             action: <ToastAction altText="fechar">Fechar</ToastAction>
         });
-        
+
         setTimeout(() => setCarregando(false), 1500);
-
-
-
-
-
         setNovoPost('');
+    }
+
+    const AddPostEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+
+        if (event.key === "Enter") {
+            if (novoPost.length === 0) {
+                return toast({
+                    title: 'Não foi possivel fazer a publicação!',
+                    description: 'Preencha campo para continuar...',
+                    action: <ToastAction altText="fechar">Fechar</ToastAction>
+                });
+            };
+            const getCurrentDate = (): string => {
+                const date = new Date();
+                const day = String(date.getDate()).padStart(2, '0'); // Pega o dia e garante dois dígitos
+                const month = String(date.getMonth() + 1).padStart(2, '0'); // Pega o mês (lembrando que os meses começam do 0)
+                const year = date.getFullYear(); // Pega o ano
+
+                return `${year}-${month}-${day}`; // Formata a data no padrão Dia-Mês-Ano
+            };
+            setCarregando(true);
+
+            setListaPosts([
+                {
+                    id: listaMeusPosts.length + 1,
+                    nome1: 'João Luis',
+                    nome2: '@jao.luisrm_',
+                    inicial: 'JL',
+                    data: getCurrentDate(),
+                    conteudo: novoPost,
+                    img: 'https://avatars.githubusercontent.com/u/124599?v=4',
+                    datas: {
+                        comentarios: 0,
+                        republicacao: 0,
+                        likes: 0,
+                        viws: 0,
+                        compartilhamentos: 0
+                    }
+                }, ...listaMeusPosts]);
+
+            toast({
+                title: 'Post postado com sucesso!',
+                action: <ToastAction altText="fechar">Fechar</ToastAction>
+            });
+
+            setTimeout(() => setCarregando(false), 1500);
+            setNovoPost('');
+        }
+
+
+
     }
 
     return (
@@ -79,6 +142,7 @@ export const ConteudoHome = () => {
                     </Avatar>
                     <Input
                         value={novoPost}
+                        onKeyDown={AddPostEnter}
                         onChange={e => setNovoPost(e.target.value)}
                         className="flex-1 border-0  focus-visible:ring-0 text-lg"
                         placeholder="Oque está acontecendo?">
@@ -86,7 +150,7 @@ export const ConteudoHome = () => {
                 </div>
                 <div className="flex justify-between pt-5">
                     <IconeInput></IconeInput>
-                    <Button onClick={Addpost} className="rounded-full font-bold">Postar</Button>
+                    <Button disabled={carregando} onClick={AddPost} className="rounded-full font-bold">Postar</Button>
                 </div>
             </div>
             {carregando &&
@@ -96,8 +160,20 @@ export const ConteudoHome = () => {
                 </div>
             }
 
-
-            {listaPosts.map(post => (
+            {!carregando && listaMeusPosts.map(post => (
+                <PostModelo
+                    id={post.id}
+                    nome1={post.nome1}
+                    nome2={post.nome2}
+                    data={post.data}
+                    conteudo={post.conteudo}
+                    img={post.img}
+                    key={post.id}
+                    datas={post.datas}
+                    inicial={post.inicial}
+                />
+            ))}
+            {!carregando && listaPosts2.map(post => (
                 <PostModelo
                     id={post.id}
                     nome1={post.nome1}
